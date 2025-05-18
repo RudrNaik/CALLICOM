e82ec55f368326d72723808272c9d40b2d6f4f7f
Rudra Naik <rudranai@iastate.edu>
2025-05-18 06:53:12 -0500

    trying something else?

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = ({}) => {
         ${scrolled ? "bg-neutral-900 shadow-md" : "bg-transparent"}
       `}
     >
-      <div className="flex items-center relative w-full font-[Geist_Mono]">
+      <div className="flex items-center relative w-full font-geist">
         <img src={SpinyLogo} alt="Logo" className="h-20" />
         <div className="text-xl px-2">SpinyNA Studios</div>
 
