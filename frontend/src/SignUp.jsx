5cdba8e15753704831027161f0ece447d43e7a87
Rudra Naik <rudranai@iastate.edu>
2025-05-24 15:26:37 -0500

    Some theoretical changes that may be able to fix the issue?

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -55,7 +55,6 @@ function SignUp() {
       if (response.ok && data.success) {
         alert("Account created!");
         setIsLoggedIn(true);
-        navigate("/login");
       } else {
         alert("Signup failed: " + (data.message || "Unknown error"));
       }
