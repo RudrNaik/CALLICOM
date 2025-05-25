13c7e53c0c97ea56fe30daed08f22f77621caf14
Rudra Naik <rudranai@iastate.edu>
2025-05-25 08:43:37 -0500

    Fixed issue where backend would not recieve updates when changing emergency dice.

--- frontend/src/components/CharacterRoster/CharacterRoster.jsx
+++ frontend/src/components/CharacterRoster/CharacterRoster.jsx
@@ -5,13 +5,15 @@ import CharacterDetail from "./CharacterDetail";
 function CharacterRoster({ userId }) {
   const [characters, setCharacters] = useState([]);
   const [selectedCharacter, setSelectedCharacter] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const [refreshFlag, setRefreshFlag] = useState(false);
 
   useEffect(() => {
-    triggerRefresh();  // Fetch the latest data when the view appears
+      triggerRefresh(); 
   }, []);
-  
+
   useEffect(() => {
+    setIsLoading(true);
     fetch(`https://callicom.onrender.com/api/characters/${userId}`)
       .then((res) => res.json())
       .then((data) => {
@@ -23,6 +25,7 @@ function CharacterRoster({ userId }) {
           );
           if (updated) setSelectedCharacter(updated);
         }
+        setIsLoading(false);
       })
       .catch(console.error);
   }, [userId, refreshFlag]);
@@ -47,9 +50,20 @@ function CharacterRoster({ userId }) {
   };
 
   return (
-    <div className="max-w-6xl mx-auto p-6 space-y-4  text-white border-l-6 border-orange-500 bg-neutral-800/30" style={{ fontFamily: 'Geist_Mono' }}>
+    <div
+      className="max-w-6xl mx-auto p-6 space-y-4  text-white border-l-6 border-orange-500 bg-neutral-800/30"
+      style={{ fontFamily: "Geist_Mono" }}
+    >
       <h1 className="text-2xl font-bold text-orange-400">Your Characters</h1>
-
+      {/* Saving Indicator */}
+      {isLoading ? (
+        <div className="flex items-center text-orange-400 font-bold py-2">
+          <div className="w-4 h-4 border-t-3 border-solid border-orange-500 rounded-3xl animate-spin mr-2"></div>
+          Fetching Operators...
+        </div>
+      ) : (
+        <div className="text-orange-400 font-bold py-2"> Operators Updated.</div>
+      )}
       <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
         {characters.map((char) => (
           <CharacterCard
