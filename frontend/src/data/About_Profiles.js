ff8e7a103f9b000cc326821f34efe48505e64d0b
Rudra Naik <rudranai@iastate.edu>
2025-08-26 14:21:08 -0500

    Some changes for the about page.

--- frontend/src/data/About_Profiles.js
+++ frontend/src/data/About_Profiles.js
@@ -8,14 +8,14 @@ const profiles = [
         "title": "Calamari Founder, Developer, Artist",
         "alt": "Picture of Spiny",
         "image": Spiny,
-        "bio": "Spiny is a Computer Science Student at [REDACTED]. He is the current developer of the Calamari website alongside esteemed artist- That Guy Gooders, and a few others."
+        "bio": "Spiny is a Computer Science Student at [REDACTED]. He is the current developer of the Calamari website alongside esteemed artist- That Guy Gooders, and a few others. He also is working on other projects- such as SEAD, a 2D flight action game. Coming soon(tm)"
     },
     {
         "name": "That Guy Gooders",
         "title": "Black Lotus Founder, Artist",
         "alt": "Picture of Gooders",
         "image" : Gooders,
-        "bio": "Gooders is a concept artist from the UK, and is the current creator of BLACK LOTUS. As well, he has helped with some of the art and writing for Calamari, and is currently helping run the campaigns."
+        "bio": "Gooders is a concept artist from the UK, and is the current creator of BLACK LOTUS. As well, he has helped with some of the art and writing for Calamari, and is the Co-DM for the current active campaign."
     }
 ];
 
