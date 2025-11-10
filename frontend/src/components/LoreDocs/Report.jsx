a9374bc97b1414a751acd9f56c501e3a92c39a62
Rudra Naik <rudranai@iastate.edu>
2025-11-10 13:18:32 -0600

    waow, another emergency commit. Now about a whitespace  preline wrap that went missing.

--- frontend/src/components/LoreDocs/Report.jsx
+++ frontend/src/components/LoreDocs/Report.jsx
@@ -5,7 +5,7 @@ function IntelReport({title, image, fluff, content}){
     return (
         <div className="cols-span-3 bg-neutral-900/85 p-5">
             <h1 className="border-orange-500 border-l-8 px-2 text-5xl">{title}</h1>
-            <h3 className="border-neutral-400 border-l-4 px-2 mt-2 text-md">{fluff}</h3>
+            <h3 className="border-neutral-400 border-l-4 px-2 mt-2 text-md whitespace-pre-line">{fluff}</h3>
             <p className="text-xs text-neutral-100 whitespace-pre-line mt-2 mb-4">
                 {content}
             </p>
